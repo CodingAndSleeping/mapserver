786df1b6da6f2b50b4aee836b90caa02b2874f05
lizhitao <1162300237@qq.com>
2022-09-02 16:44:51 +0800

    mapcache

--- mapserver-demo/src/components/mapIndex.vue
+++ mapserver-demo/src/components/mapIndex.vue
@@ -4,6 +4,7 @@ import Map from "ol/Map";
 import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
 import TileWMS from "ol/source/TileWMS";
+import WMTS from 'ol/source/WMTS';
 import View from "ol/View";
 import { onMounted, ref } from "vue";
 import {
@@ -27,63 +28,67 @@ function initMap() {
 
   wmsLayer.value = new TileLayer({
     name: "wms",
-    source: new TileWMS({
-      url: "http://127.0.0.1:8181/cgi-bin/mapserv.exe",
-      params: {
-        layers: [
-          "LNDRGN_A",
-          "LNDARE_A",
-          "SBDARE_A", 
-          "DMPGRD",
-          "LAKARE",
-          "RIVERS_A",
-          "RESARE",
-          "DEPARE_A1",
-          "DEPARE_A2",
-          "DEPARE_A3",
-          "DEPARE_A4",
-          "OBSTRN_A",
-          "SLCONS_L",
-          "COALNE",
-          "DEPCNT",
-          "LIGHTS",       
-          "RTPBCN",
-          "ROADWY",
-          "WATTUR_L",
-          "MAGVAR_L",
-          "RAILWY",
-          "DEPARE",
-          "RIVERS",
-          "SLCONS_P",
-          "LNDARE_P",
-          "RDOSTA",
-          "SBDARE_P",
-          "LNDMRK",
-          "OFSPLF",
-          "CURENT",
-          "WRECKS1",
-          "WRECKS2",
-          "WRECKS5",
-          "TS_FEB1",
-          "TS_FEB2",
-          "MAGVAR_P",
-          "LNDRGN_P",
-          "BCNSPP",
-          "CTNARE", 
-          "UWTROC3",
-          "UWTROC4",
-          "UWTROC5",
-          "FORSTC",
-          "BUAARE",
-          "WATTUR",
-          "OBSTRN9",
-          "OBSTRN_N",
-        ],
-        MAP: "D:/mapserver/local.map",
-        TILED: true,
-      },
-      serverType: "mapserver",
-    }),
+    source: new WMTS({
+      url:"http://127.0.0.1:8181/mapcache/tms/1.0.0/{TileMatrixSet}/{TileMatrix}/{TileRow}/{TileCol}.png"
+    })
+    // source: new TileWMS({
+    //   url: "http://127.0.0.1:8181/cgi-bin/mapserv.exe",
+    //   params: {
+    //     layers: [
+    //       "LNDRGN_A",
+    //       "LNDARE_A",
+    //       "SBDARE_A", 
+    //       "DMPGRD",
+    //       "LAKARE",
+    //       "RIVERS_A",
+    //       "RESARE",
+    //       "DEPARE_A1",
+    //       "DEPARE_A2",
+    //       "DEPARE_A3",
+    //       "DEPARE_A4",
+    //       "OBSTRN_A",
+    //       "SLCONS_L",
+    //       "COALNE",
+    //       "DEPCNT",
+    //       "LIGHTS",       
+    //       "RTPBCN",
+    //       "ROADWY",
+    //       "WATTUR_L",
+    //       "MAGVAR_L",
+    //       "RAILWY",
+    //       "DEPARE",
+    //       "RIVERS",
+    //       "SLCONS_P",
+    //       "LNDARE_P",
+    //       "RDOSTA",
+    //       "SBDARE_P",
+    //       "LNDMRK",
+    //       "OFSPLF",
+    //       "CURENT",
+    //       "WRECKS1",
+    //       "WRECKS2",
+    //       "WRECKS5",
+    //       "TS_FEB1",
+    //       "TS_FEB2",
+    //       "MAGVAR_P",
+    //       "LNDRGN_P",
+    //       "BCNSPP",
+    //       "CTNARE", 
+    //       "UWTROC3",
+    //       "UWTROC4",
+    //       "UWTROC5",
+    //       "FORSTC",
+    //       "BUAARE",
+    //       "WATTUR",
+    //       "OBSTRN9",
+    //       "OBSTRN_N",
+    //     ],
+    //     MAP: "D:/mapserver/local.map",
+    //     TILED: true,
+    //   },
+    //   serverType: "mapserver",
+    // }),
+    
   });
   map.value = new Map({
     // 存放地图的容器
@@ -107,6 +112,7 @@ function initMap() {
 }
 onMounted(() => {
   initMap();
+
 });
 </script>
 
